2d943f6feac617ade05e95f74f05251cc3508c2a
HyeonjoonNam-DigiPen <guswns409@naver.com>
2023-05-28 14:08:34 +0900

    Revert "Merge branch 'HyeonjoonNam' of https://github.com/KORJUNH/Suspense_defense into HyeonjoonNam"
    This reverts commit b830634298a49d37e934ea7184947ba8392365ed, reversing
    changes made to 3eb01ad1afa799e4ba8831675cf45a7d98fa5f82.

--- new_GAM150/Game/Tile.h
+++ new_GAM150/Game/Tile.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../Engine/Sprite.h"
 #include "../Engine/Vec2.h"
 #include "doodle/drawing.hpp"
 using namespace doodle;
@@ -31,8 +30,6 @@ public:
 	int GetHP() { return hp; }
 	// Reduce hp of the tile
 	void ReduceHP() { --hp; }
-
-	
 };
 // Wall tile
 class Wall : public Tile {
@@ -41,12 +38,6 @@ public:
 	void Update();
 	void Draw(bool is_day);
 	virtual void Attacked();
-	enum class colonycore_animations
-	{
-		basic,
-		wallattacked,
-		broken
-	};
 };
 // Void tile
 class Void : public Tile {
@@ -62,14 +53,6 @@ public:
 	void Update();
 	void Draw(bool is_day);
 	virtual void Attacked();
-	enum class colonycore_animations
-	{
-		basic,
-		attacked1,
-		attacked2,
-		attacked3,
-		attacked4
-	};
 };
 // Colony_Side tile
 class Colony_Side : public Tile {
@@ -104,12 +87,6 @@ public:
 	Resource(Math::vec2 position);
 	void Update();
 	void Draw(bool is_day);
-	enum class resource_animations
-	{
-		basic,
-		resourceattacked,
-		broken
-	};
 };
 // Warp tile
 class Warp :public Tile {
