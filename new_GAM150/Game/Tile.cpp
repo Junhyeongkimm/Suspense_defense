8011a02a0fed4215a37ccbb06639d118501cf0f4
KORJUNH <kjhmj3016@naver.com>
2023-06-01 14:53:34 +0900

    test
    test

--- new_GAM150/Game/Tile.cpp
+++ new_GAM150/Game/Tile.cpp
@@ -21,7 +21,7 @@ void Tile::Draw(bool is_day) {
 int Tile::Get_State() {
 	return state;
 }
-// Check attcked
+// Check attcked6
 void Tile::Attacked(Math::vec2 attack_point) {
 	if (attack_point.x > position.x && attack_point.x < position.x + size && attack_point.y > position.y && attack_point.y < position.y + size) {
 		--hp;
@@ -38,31 +38,50 @@ bool Tile::Attacked(Math::vec2 attack_point, int i) {
 // ---------------------------------------------------------------------------------------------------
 // Tiles below are very similar. The major difference is hp and state.
 
+
+#include <iostream>
+
 // Wall
 Wall::Wall(Math::vec2 position) : Tile(position) {
 	tiles.push_back(this);
 	state = TILES::WALL;
 	hp = 2;
+
+	sprite.Load("Assets/rock.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(rock_animations::basic));
 }
 void Wall::Update() {
-
+	/*if (hp == 1 && rockbroken == false){
+		rockbroken = true;
+		std::cout << "broken \n";
+		sprite.PlayAnimation(static_cast<int>(rock_animations::broken));
+	}*/
 }
 void Wall::Draw(bool is_day) {
 	doodle::push_settings();
-	if (is_day) {
-		if (hp == 2)
-			set_fill_color(HexColor{ 0x444444ff });
-		else if (hp == 1)
-			set_fill_color(HexColor{ 0x888888ff });
+	//if (is_day) {
+	//	if (hp == 2)
+	//		set_fill_color(HexColor{ 0x444444ff });
+	//	else if (hp == 1){}
+	//		set_fill_color(HexColor{ 0x888888ff });
+
+	//}
+	//else {
+	//	if (hp == 2)
+	//		/*set_fill_color(HexColor{ 0x000000ff });*/
+	//	else if (hp == 1)
+	//		/*set_fill_color(HexColor{ 0x444444ff }*/);
+	//}
+	//draw_rectangle(position.x, position.y, size, size);
+	if (hp == 1 && rockbroken == false) {
+		rockbroken = true;
+		std::cout << "broken \n";
+		sprite.PlayAnimation(static_cast<int>(rock_animations::broken));
 	}
-	else {
-		if (hp == 2)
-			set_fill_color(HexColor{ 0x000000ff });
-		else if (hp == 1)
-			set_fill_color(HexColor{ 0x444444ff });
-	}
-	
-	draw_rectangle(position.x, position.y, size, size);
+
+	sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	doodle::pop_settings();
 }
 void Wall::Attacked() {
@@ -71,6 +90,10 @@ void Wall::Attacked() {
 // Void
 Void::Void(Math::vec2 position) : Tile(position) {
 	state = TILES::VOID;
+	sprite.Load("Assets/tile.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(void_animations::basic));
 }
 void Void::Update() {
 
@@ -78,13 +101,12 @@ void Void::Update() {
 void Void::Draw(bool is_day) {
 	doodle::push_settings();
 	if (is_day) {
-		set_fill_color(HexColor{ 0xffffffff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
 	else {
-		set_fill_color(HexColor{ 0xbbbbbbff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
-	
-	draw_rectangle(position.x, position.y, size, size);
+
 	doodle::pop_settings();
 }
 // Colony_Core
@@ -103,7 +125,7 @@ void Colony_Core::Draw(bool is_day) {
 	else {
 		set_fill_color(HexColor{ 0x440000ff });
 	}
-	
+
 	draw_rectangle(position.x, position.y, size, size);
 	doodle::pop_settings();
 }
@@ -126,7 +148,7 @@ void Colony_Side::Draw(bool is_day) {
 	else {
 		set_fill_color(HexColor{ 0x220000ff });
 	}
-	
+
 	draw_rectangle(position.x, position.y, size, size);
 	doodle::pop_settings();
 }
@@ -137,6 +159,10 @@ void Colony_Side::Attacked() {
 Base_Wall::Base_Wall(Math::vec2 position) : Tile(position) {
 	state = TILES::BASE_WALL;
 	hp = 10;
+	sprite.Load("Assets/wall.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(basewall_animations::basic));
 }
 void Base_Wall::Update() {
 
@@ -144,13 +170,12 @@ void Base_Wall::Update() {
 void Base_Wall::Draw(bool is_day) {
 	doodle::push_settings();
 	if (is_day) {
-		set_fill_color(HexColor{ 0x496f35ff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
 	else {
-		set_fill_color(HexColor{ 0x052c01ff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
-	
-	draw_rectangle(position.x, position.y, size, size);
+
 	doodle::pop_settings();
 }
 void Base_Wall::Attacked() {
@@ -159,78 +184,95 @@ void Base_Wall::Attacked() {
 // Base_Inside
 Base_Inside::Base_Inside(Math::vec2 position) : Tile(position) {
 	state = TILES::BASE_INSIDE;
+	sprite.Load("Assets/wallinside.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(baseinside_animations::basic));
 }
 void Base_Inside::Update() {
-	
+
 }
 void Base_Inside::Draw(bool is_day) {
 	doodle::push_settings();
 	if (is_day) {
-		set_fill_color(HexColor{ 0xffffffff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
 	else {
-		set_fill_color(HexColor{ 0xbbbbbbff });
+		sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	}
-	
-	draw_rectangle(position.x, position.y, size, size);
 	doodle::pop_settings();
 }
 // Resource
 Resource::Resource(Math::vec2 position) : Tile(position) {
 	state = TILES::RESOURCE;
-	hp = 1;
+	hp = 3;
+	sprite.Load("Assets/resource.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(resource_animations::basic));
 }
 void Resource::Update() {
-	
+
 }
 void Resource::Draw(bool is_day) {
 	doodle::push_settings();
-	if (is_day) {
+	/*if (is_day) {
 		set_fill_color(HexColor{ 0xffd966ff });
 	}
 	else {
 		set_fill_color(HexColor{ 0xbb9522ff });
-	}
-	
-	draw_rectangle(position.x, position.y, size, size);
+	}*/
+	sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	doodle::pop_settings();
+}
+void Resource::Attacked() {
+	--hp;
 }
 // Warp
 Warp::Warp(Math::vec2 position) : Tile(position) {
 	state = TILES::WARP;
-	hp = 1;
+	hp = 2;
+	sprite.Load("Assets/portalresource.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(warp_resource_animations::basic));
 }
 void Warp::Update() {
 
 }
 void Warp::Draw(bool is_day) {
 	push_settings();
-	if (is_day) {
+	/*if (is_day) {
 		set_fill_color(HexColor{ 0x0000ffff });
 	}
 	else {
 		set_fill_color(HexColor{ 0x0000ffff });
 	}
 
-	draw_rectangle(position.x, position.y, size);
+	draw_rectangle(position.x, position.y, size);*/
+	sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	pop_settings();
 }
 // Tower
 Tower::Tower(Math::vec2 position) : Tile(position) {
 	state = TILES::TOWER;
-	hp = 10;
+	hp = 10; sprite.Load("Assets/basecore.spt");
+	scale_x = size / static_cast<double>(sprite.GetFrameSize().x);
+	scale_y = size / static_cast<double>(sprite.GetFrameSize().y);
+	sprite.PlayAnimation(static_cast<int>(Tower_animations::basic));
+
 }
 void Tower::Update() {
 
 }
 void Tower::Draw(bool is_day) {
 	push_settings();
-	if (is_day) {
+	/*if (is_day) {
 		set_fill_color(HexColor{ 0x0000ffff });
 	}
 	else {
 		set_fill_color(HexColor{ 0x0000ffff });
-	}
-	draw_rectangle(position.x, position.y, size);
+	}*/
+	sprite.Draw((Math::TranslationMatrix(position) * Math::ScaleMatrix({ scale_x, scale_y })));
 	pop_settings();
 }
--- new_GAM150/Game/Tile.h
+++ new_GAM150/Game/Tile.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../Engine/Sprite.h"
 #include "../Engine/Vec2.h"
 #include "doodle/drawing.hpp"
 using namespace doodle;
@@ -7,98 +8,150 @@ using namespace doodle;
 // enum TILES { WALL, VOID, COLONY_CORE, COLONY_SIDE, BASE_WALL, BASE_INSIDE, RESOURCE, WARP, TOWER };
 class Tile {
 protected:
-	static inline const double size = 50;
-	Math::vec2 position;
-	int state = 0;
-	int hp = 0;
-	HexColor color = 0;
+    static inline const double size = 50;
+    Math::vec2 position;
+    int state = 0;
+    int hp = 0;
+    HexColor color = 0;
+
 public:
-	// Constructor
-	Tile(Math::vec2 position);
-	// Update
-	virtual void Update();
-	// Draw
-	virtual void Draw(bool is_day) = 0;
-	// Check attacked
-	 void Attacked(Math::vec2 attack_point);
-	 // Check attacked and true or false
-	 bool Attacked(Math::vec2 attack_point, int i);
-	 // Getter functions
-	Math::vec2 GetPosition() { return position; }
-	double GetDistance(Math::vec2 target) { return sqrt((position.x - target.x) * (position.x - target.x) + (position.y - target.y) * (position.y - target.y)); }
-	int Get_State();
-	int GetHP() { return hp; }
-	// Reduce hp of the tile
-	void ReduceHP() { --hp; }
+    // Constructor
+    Tile(Math::vec2 position);
+    // Update
+    virtual void Update();
+    // Draw
+    virtual void Draw(bool is_day) = 0;
+    // Check attacked
+    void Attacked(Math::vec2 attack_point);
+    // Check attacked and true or false
+    bool Attacked(Math::vec2 attack_point, int i);
+    // Getter functions
+    const Math::vec2& GetPosition() const { return position; }
+    double GetDistance(Math::vec2 target) { return sqrt((position.x - target.x) * (position.x - target.x) + (position.y - target.y) * (position.y - target.y)); }
+    int Get_State();
+    int GetHP() { return hp; }
+    CS230::Sprite sprite;
+    // Reduce hp of the tile
+    void ReduceHP() { --hp; }
+    double scale_x;
+    double scale_y;
+
 };
 // Wall tile
 class Wall : public Tile {
 public:
-	Wall(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
-	virtual void Attacked();
+    Wall(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    virtual void Attacked();
+    enum class rock_animations
+    {
+        basic,
+        broken
+    };
+private:
+    bool rockbroken = false;
 };
 // Void tile
 class Void : public Tile {
 public:
-	Void(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
+    Void(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    enum class void_animations
+    {
+        basic
+    };
 };
 // Colony_Core tile
 class Colony_Core : public Tile {
 public:
-	Colony_Core(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
-	virtual void Attacked();
+    Colony_Core(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    virtual void Attacked();
+    enum class colonycore_animations
+    {
+        basic,
+        attacked1,
+        attacked2,
+        attacked3,
+        attacked4
+    };
 };
 // Colony_Side tile
 class Colony_Side : public Tile {
 public:
-	Colony_Side(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
-	virtual void Attacked();
+    Colony_Side(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    virtual void Attacked();
 };
 // Base_Wall tile
 class Base_Wall : public Tile {
 public:
-	Base_Wall(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
-	virtual void Attacked();
-	bool AbleToBeAttacked() { if (invincibility_count > invincibility_time) { invincibility_count = 0; return true; } else { return false; } }
+    Base_Wall(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    virtual void Attacked();
+    bool AbleToBeAttacked() { if (invincibility_count > invincibility_time) { invincibility_count = 0; return true; } else { return false; } }
+    enum class basewall_animations
+    {
+        basic
+    };
 private:
-	double invincibility_count = 0;
-	const double invincibility_time = 1.0;
+    double invincibility_count = 0;
+    const double invincibility_time = 1.0;
 };
 // Base_Inside tile
 class Base_Inside : public Tile {
 public:
-	Base_Inside(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
+    Base_Inside(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    enum class baseinside_animations
+    {
+        basic
+    };
 };
 // Resource tile
 class Resource : public Tile {
 public:
-	Resource(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
+    Resource(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    virtual void Attacked();
+    enum class resource_animations
+    {
+        basic,
+        resourceattacked,
+        broken
+    };
 };
 // Warp tile
 class Warp :public Tile {
 public:
-	Warp(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
+    Warp(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    enum class warp_resource_animations
+    {
+        basic,
+        resourceattacked,
+        broken
+    };
 };
 // Tower tile
 class Tower : public Tile {
 public:
-	Tower(Math::vec2 position);
-	void Update();
-	void Draw(bool is_day);
+    Tower(Math::vec2 position);
+    void Update();
+    void Draw(bool is_day);
+    enum class Tower_animations
+    {
+        basic,
+        resourceattacked,
+        broken
+    };
+
 };
